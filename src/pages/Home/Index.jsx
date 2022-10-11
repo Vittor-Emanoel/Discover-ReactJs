89385c623c1427f0cda2414fadffc2516c996b13
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-11 17:20:04 -0300

    FEAT: add states

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Card from "../../components/Card/Index";
 import "./styles.css";
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className="container ">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome.." />
+      <h1>Lista de presença{studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome.."
+        onChange={(e) => setStudentName(e.target.value)} // pegando o valor do input e atualizando o stado
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Vittor" time="13:39:00" />
