44f14b8841ee7cac304897ca3a0ef6028b3578ec
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-10-11 13:36:31 -0300

    FEAT: Add paste Components & Create Card Compent

---
+++ src/components/Card/Index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <strong>Vittor</strong>
+      <small>10:15:24</small>
+    </div>
+  );
+};
+export default Card;
